41dee4e7d9a5a9a1285039eea8ebcb334e6059f0
KellyDolan127 <39315580+KellyDolan127@users.noreply.github.com>
2018-11-26 22:32:58 -0500

    Add files via upload
    This is just the game list which calls a request in a request, and it posts my own json object with only what we want. I'm getting my computer set up again right now but I am gonna work on this during work tomorrow and when I get home.  But yeah this is the data that will be returned to our html.  This would be for the profile subtab "myGames"

--- steam_api.js
+++ steam_api.js
@@ -13,19 +13,74 @@ var request = require('request');
 //parse through returned json
 //post to website
 
+var games = [];
+
+//easy
+//all standarard api calls:
+//Grab Info for GameList
+//Grab info for friendlist
+//grab achievements
+//grab stats
+
+//games rec:
+  //list of all games(top 100)
+  //if game not in userGames and game.genre like (blah)
+
+//medium:
+//hook all data into SQL and then query sql to display onto page
 
 
+
+
+//returns user owned games
 app.get('/steam/user', function(req, res) { //posting to our website
 
-    var url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/' +
-
-        '?key=EF6233DCF8DC8AFFB8419F82A085B4A1&steamids=76561198087236531';// +
+    ///ISteamUser/GetFriendList/v1/
+    var url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/' +
+        '?key=EF6233DCF8DC8AFFB8419F82A085B4A1&steamid=76561198087236531';// +
 
     request.get(url, function(error, steamRes, steamBody) { //getting from steam
 
+        var urlGame = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/' + '?key=EF6233DCF8DC8AFFB8419F82A085B4A1&appid='; 
 
-        res.send(steamBody);
+        var temp =JSON.stringify(steamBody);
+        var toSend ={};
+        var i = 1;
 
+        games = temp.split('appid');
+        function populateToSend(_callback){
+        games.forEach(function (elem){
+            elem = elem.replace(/^\D+/g, '');
+            var num = elem.indexOf(','); 
+            elem = elem.substring(0, num);
+            
+            request.get(urlGame + elem, function(gameError, gameSteamRes, gameSteamBody) { //getting from stea
+                var gameName = JSON.stringify(gameSteamBody);
+                var toStart =gameName.indexOf ('Name');
+                var toRemove =gameName.indexOf ('gameV');
+                gameName = gameName.substring(toStart, toRemove);
+        //console.log(toStart); console.log(toRemove);
+                gameName = gameName.replace(/['"\\,]+/g,'');
+                gameName = gameName.replace('Name:', '');
+                if (gameName != null && gameName != ''){
+                   // console.log(gameName.replace('Name:', ''));
+                    toSend["Game" + i]=gameName.replace('Name:', '');
+                    //console.log(toSend);
+                    i++;
+                    // res.send(gameName.replace('Name:', ''));
+                }
+            });
+        });
+       
+        _callback();    
+        }
+        function send(){
+            populateToSend(function(){
+            setTimeout(function(){res.send(toSend);}, 3000);
+            });
+        }
+        send();
+        
     });
 
 });
@@ -35,5 +90,5 @@ var port = 6000;
 
 var server = app.listen(port);
 
-console.log('Navigate to http://localhost:4000/steam/user');
+//console.log('Navigate to http://localhost:4000/steam/user');
 
